0f6b410a6e463927919a3cc550d8220d6f8ea5c4
Daniel <dannyboy21204@gmail.com>
2025-11-30 11:04:12 +0000

    Addition of access_control.py for Role-based access control. The implementation of the access control in main menu for View All Registered Users Function.

--- app/main.py
+++ app/main.py
@@ -3,11 +3,16 @@ Main menu for the text-based School Evaluation Platform
 
 User Stories included so far:
  - US21 - User Registration
+ - US22 - User Login
+ - US23 - Session Handling
+ - US31 - Global Session Handling
+ - US25 - Role based access Control
 """
 
 from app.auth import register_user, login_user
 from app.data_store import get_users, example_users, get_current_user, clear_current_user
 from app.validation import validate_menu_option_format
+from app.access_control import user_has_role, check_access, ROLE_ADMIN
 
 def show_main_menu():
     """
@@ -28,12 +33,14 @@ def show_main_menu():
         print(f"Logged in as {current.get('username')}")
 
     print("1. Register new User")
-    print("2. Display Registered Users (debug)")
 
     if current is None:
-        print("3. Login")
+        print("2. Login")
     else:
-        print("3. Logout")
+        print("2. Logout")
+
+    if current is not None and user_has_role(current, [ROLE_ADMIN]):
+        print("9. Display Registered Users (Admin Only)")
 
     print("0. Exit")
 
@@ -79,26 +86,25 @@ def main() -> None:
 
     while True:
         show_main_menu()
-        choice = input("Select an option: ").strip()
+        current = get_current_user()
 
-        allowed_options = ["1", "2", "3", "0"]
+        allowed_options = ["1", "2", "0"]
+
+        if current is not None and user_has_role(current, [ROLE_ADMIN]):
+            allowed_options.append("9")
+
+        choice = input("Select an option: ").strip()
 
         valid, msg = validate_menu_option_format(choice, allowed_options)
         if not valid:
             print(msg)
             continue
 
-        current = get_current_user()
-
         if choice == "1":
             #US21 - User Registration
             register_user()
 
         elif choice == "2":
-            # Shows all users currently stored in the system
-            list_users_debug()
-
-        elif choice == "3":
             if current is None:
                 # US23 - Log in Feature
                 login_user()
@@ -106,6 +112,12 @@ def main() -> None:
                 # US23 - Log out feature
                 clear_current_user()
                 print("\nYou have been logged out")
+
+        elif choice == "9":
+            # Shows all users currently stored in the system
+            if not check_access(current, [ROLE_ADMIN], print_func=print):
+                continue
+            list_users_debug()
 
         elif choice == "0":
             print("\nThank you for using School Evaluation Platform")
