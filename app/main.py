6c58ef562ffd3226f2a5119e617fda7da36757df
SRDurrant <simondurrant12@virginmedia.com>
2025-12-17 23:55:46 +0000

    Added system statistics telling number of users, schools, ratings and comments and white/blackbox testing for it

--- app/main.py
+++ app/main.py
@@ -18,7 +18,8 @@ from app.auth import register_user, login_user, reset_password
 from app.data_store import get_users, example_users, get_current_user, clear_current_user
 from app.validation import validate_menu_option_format
 from app.access_control import user_has_role, check_access, ROLE_ADMIN
-from app.admin_actions import list_all_users, delete_user_by_id, add_new_school, delete_comment_by_id, delete_school_by_id, update_school_by_id
+from app.admin_actions import list_all_users, delete_user_by_id, add_new_school, delete_comment_by_id, \
+    delete_school_by_id, update_school_by_id, view_system_statistics
 from app.school_actions import list_all_schools
 
 def show_main_menu():
@@ -67,6 +68,9 @@ def show_main_menu():
     if current is not None and user_has_role(current, [ROLE_ADMIN]):
         print("11. Delete a Comment (Admin Only)")
 
+    if current is not None and user_has_role(current, [ROLE_ADMIN]):
+        print("12. View System Statistics (Admin Only)")
+
     print("0. Exit")
 
 
@@ -100,6 +104,7 @@ def main() -> None:
             allowed_options.append("9")
             allowed_options.append("10")
             allowed_options.append("11")
+            allowed_options.append("12")
 
         choice = input("Select an option: ").strip()
 
@@ -177,6 +182,11 @@ def main() -> None:
                 continue
 
             delete_comment_by_id(cid, print)
+
+        elif choice == "12":
+            if not check_access(current, [ROLE_ADMIN], print_func=print):
+                continue
+            view_system_statistics(print)
 
         elif choice == "0":
             print("\nThank you for using School Evaluation Platform")
